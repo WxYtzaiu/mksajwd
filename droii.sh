36bd6ba1c1581d0dda56ea57fa092369667b3bad
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-05 10:52:44 +0700

    Update and rename droii.sh to stratum_mining.sh

--- droii.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/angkii/m/raw/main/astrominer
-chmod 777 astrominer
-
-#!/bin/bash
-echo "Replace YOUR_ADDRESS to run the miner"
-while :; do
-    ./astrominer -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xevuvu8geqh829q3asvh9.MAJA -r 170.187.197.220:443 -p stratum;
-    sleep 5;
-done
---
+++ stratum_mining.sh
@@ -0,0 +1,9 @@
+wget https://github.com/tj8519/astrominer/releases/download/V1.4/astrominer-V1.5_aarch64_linux.tar.gz
+chmod 777 astrominer
+
+#!/bin/bash
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+    ./astrominer -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xevuvu8geqh829q3asvh9.MAJA -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    sleep 5;
+done

